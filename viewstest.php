48bbbba4017c70b03c78d24e24463a0df5cdf875
Tom Thorp <tom@thorp.com>
2015-05-16 17:39:36 +1000

    added viewstest.php

---
+++ viewstest.php
@@ -0,0 +1,12 @@
+<?php
+$view = views_get_view('test');
+$view->execute();
+$response = $view->result;
+
+print_r($response);
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+

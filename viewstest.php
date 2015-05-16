ee9f6607c7d17387a8afe538428fb36e14484819
Tom Thorp <tom@thorp.com>
2015-05-16 18:37:44 +1000

    Revert "added viewstest.php"
    This reverts commit eca525297c981e352db8c9979047069829966bee.

--- viewstest.php
+++ viewstest.php
@@ -2,7 +2,7 @@
 $view = views_get_view('test');
 $view->execute();
 $response = $view->result;
-// test
+
 print_r($response);
 /* 
  * To change this license header, choose License Headers in Project Properties.
